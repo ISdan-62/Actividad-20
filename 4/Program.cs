86ffd58ccc2655ae784d35926a2be22ebf93caf0
erick <erick@LEGI0SORIO>
2026-04-09 23:01:55 -0600

    4

---
+++ 4/Program.cs
@@ -0,0 +1,21 @@
+﻿int cantidadNumeros;
+int suma = 0;
+
+Console.Write("Ingrese la cantidad de numeros: ");
+cantidadNumeros = int.Parse(Console.ReadLine());
+
+int[] arregloNumeros = new int[cantidadNumeros];
+
+for (int i = 0; i < arregloNumeros.Length; i++)
+{
+    Console.Write("Ingrese el numero: ");
+    arregloNumeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < arregloNumeros.Length; i++)
+{
+    suma += arregloNumeros[i];
+}
+
+double promedio = (double)suma / arregloNumeros.Length;
+Console.WriteLine("El promedio es: " + promedio);
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿int cantidadNumeros;
+
+Console.Write("Ingrese la cantidad de numeros: ");
+cantidadNumeros = int.Parse(Console.ReadLine());
+
+int[] arregloNumeros = new int[cantidadNumeros];
+
+for (int i = 0; i < arregloNumeros.Length; i++)
+{
+    Console.Write("Ingrese el numero: ");
+    arregloNumeros[i] = int.Parse(Console.ReadLine());
+}
+
+int numeroMayor = arregloNumeros[0];
+
+for (int i = 1; i < arregloNumeros.Length; i++)
+{
+    if (arregloNumeros[i] > numeroMayor)
+    {
+        numeroMayor = arregloNumeros[i];
+    }
+}
+
+Console.WriteLine($"El numero mayor es: {numeroMayor}");
