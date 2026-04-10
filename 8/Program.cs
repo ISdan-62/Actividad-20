51cd1e2df5b764a6bdef18a168d9b8df0eb6feee
erick <erick@LEGI0SORIO>
2026-04-09 23:20:26 -0600

    8

---
+++ 8/Program.cs
@@ -0,0 +1,22 @@
+﻿int cantidadNumeros;
+int numerosImpares = 0;
+
+Console.Write("Ingrese la cantidad de numeros: ");
+cantidadNumeros = int.Parse(Console.ReadLine());
+
+int[] arregloNumeros = new int[cantidadNumeros];
+
+for (int i = 0; i < arregloNumeros.Length; i++)
+{
+    Console.Write("Ingrese el numero: ");
+    arregloNumeros[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < arregloNumeros.Length; i++)
+{
+    if (arregloNumeros[i] % 2 != 0)
+    {
+        numerosImpares++;
+    }
+}
+
+Console.WriteLine($"Cantidad de numeros impares: {numerosImpares}");
