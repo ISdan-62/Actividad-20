d794e34ba70001cb089b7db7e9fc29af6deaa075
erick <erick@LEGI0SORIO>
2026-04-09 23:40:23 -0600

    10

---
+++ 10/Program.cs
@@ -0,0 +1,25 @@
+﻿int cantidadNumeros;
+
+Console.Write("Ingrese la cantidad de numeros: ");
+cantidadNumeros = int.Parse(Console.ReadLine());
+
+int[] arregloNumeros = new int[cantidadNumeros];
+
+for (int i = 0; i < arregloNumeros.Length; i++)
+{
+    Console.Write("Ingrese el numero: ");
+    arregloNumeros[i] = int.Parse(Console.ReadLine());
+}
+Console.Write("Ingrese la posicion: ");
+int posicion = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el nuevo valor: ");
+int nuevo = int.Parse(Console.ReadLine());
+
+arregloNumeros[posicion] = nuevo;
+
+Console.WriteLine("Arreglo actualizado:");
+for (int i = 0; i < arregloNumeros.Length; i++)
+{
+    Console.Write(arregloNumeros[i] + " ");
+}
